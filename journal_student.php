c3a91418d9be7e603cedb185da0a0d229240304f
Galina <mikyhinamori@gmail.com>
2023-04-06 22:23:48 +0300

    Add journalPrototype

---
+++ journal_student.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/shedule_student.css">
+    <title>Журнал</title>
+</head>
+<body>
+    <div class="content">
+        <div class="journal_container">
+            <div class = "journal_content">
+                <div class="menu">
+                    <div class="choice">
+                        <select name="year" class="year">
+                            <option value="s1">2023</option>
+                        </select> 
+                        <select name="month" class="month">
+                            <option value="s1">Сентябрь</option>
+                        </select> 
+                    </div>
+                    <div class="buttons">
+                        <button class="btn1">К предметам</button>
+                        <button class="btn2">Вернуться в личный кабинет</button>
+                    </div>
+                </div>
+                <div class="calendar">
+                    <div class="card">
+                        <div class="card_content">
+                            <div class="date">
+                                <span>1 - Пн</span>
+                            </div>
+                            <div class="mark">
+                                <span>Не явился</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
